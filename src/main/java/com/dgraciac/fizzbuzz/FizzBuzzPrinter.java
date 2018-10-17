905b46d63a0c334a19b920d3953bd0524f7262b3
David Gracia <david.gracia.celemendi@gmail.com>
2018-10-17 10:12:04 +0200

    changes

---
+++ src/main/java/com/dgraciac/fizzbuzz/FizzBuzzPrinter.java
@@ -0,0 +1,10 @@
+package com.dgraciac.fizzbuzz;
+
+public class FizzBuzzPrinter {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        for (int i = 1; i < 101; i++) {
+            System.out.println(fizzBuzz.compute(i));
+        }
+    }
+}
